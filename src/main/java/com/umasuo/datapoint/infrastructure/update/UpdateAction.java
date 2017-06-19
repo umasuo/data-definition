a81c3cea389dd33150099304d59c4bbcfbdc04e1
davis999 <davis.dai@go6d.com>
2017-06-19 15:05:05 +0800

    add update package

---
+++ src/main/java/com/umasuo/datapoint/infrastructure/update/UpdateAction.java
@@ -0,0 +1,24 @@
+package com.umasuo.datapoint.infrastructure.update;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.io.Serializable;
+
+/**
+ * configurations for common update actions that will be used in more thant one service
+ * and this action also extends other action configure in each service.
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property =
+    "action")
+@JsonSubTypes( {
+
+})
+public interface UpdateAction extends Serializable {
+  /**
+   * get action name.
+   *
+   * @return name in string.
+   */
+  String getActionName();
+}
---
+++ src/main/java/com/umasuo/datapoint/infrastructure/update/UpdateActionUtils.java
@@ -0,0 +1,12 @@
+package com.umasuo.datapoint.infrastructure.update;
+
+/**
+ * update action util.
+ */
+public class UpdateActionUtils {
+
+  /**
+   * add data definition.
+   */
+  public static final String CHANGE_OPENABLE = "changeOpenable";
+}
